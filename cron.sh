bf2874326c3903e8edd26ceb5d049f7fab2ee698
Leo Lapworth <leo@cuckoo.org>
2016-05-08 19:03:32 +0100

    Better check.. only look at the last line

--- cron.sh
+++ cron.sh
@@ -3,4 +3,4 @@
 # Script to run in cron for production, once puppet has set everything up
 # Only produces output if the `Result:` line does not contain `PASS`
 
-~/bin/metacpan-monitoring-carton exec ~/carton/metacpan-monitoring/bin/swat | grep Result: | grep -v PASS
+~/bin/metacpan-monitoring-carton exec ~/carton/metacpan-monitoring/bin/swat | tail -1 | grep -v 'Result: PASS'
