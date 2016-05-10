b78dde6edd474983c5be249e67544738b3007683
Alexey Melezhik <melezhik@gmail.com>
2016-05-11 00:54:19 +0300

    clear swat cache files after swat is done

--- cron.sh
+++ cron.sh
@@ -4,6 +4,7 @@
 # Only produces output if the last `Result:` line does not contain `PASS`
 
 export SWAT_FILE="/tmp/last_swat_run"
+export swat_purge_cache=1
 
 # Run and save output to tmp
 ~/bin/metacpan-monitoring-carton exec ~/carton/metacpan-monitoring/bin/swat > $SWAT_FILE
