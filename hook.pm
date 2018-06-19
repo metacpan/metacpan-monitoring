ac7eeda7b29a2cd45546f27f771a57b1aae42d8e
Alexey Melezhik <melezhik@gmail.com>
2018-06-19 15:53:07 +0000

    adding sitemap authors check #5

--- hook.pm
+++ hook.pm
@@ -3,4 +3,4 @@ use metacpan::monitoring;
 run_swat_module( GET => '/recent' );
 run_swat_module( GET => '/search' );
 run_swat_module( GET => '/feed/recent' );
-
+run_swat_module( META => 'sitemap-authors-xml/check' );
--- sitemap-authors-xml/check/hook.pm
+++ sitemap-authors-xml/check/hook.pm
@@ -1,9 +1,8 @@
 run_swat_module( GET => "sitemap-authors.xml.gz" );
 
-
+my $j=0;
 for my $i (our @site_map_url){
-  set_response("$i ...");
+  last if $j++>3;
   run_swat_module( GET => "sitemap-authors-xml/check-author" , { url => $i });
 }
 
-set_response("FINISHED");
