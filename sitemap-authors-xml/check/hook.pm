846d13c0df762035018ec5939e4b4a32d81be490
Alexey Melezhik <melezhik@gmail.com>
2018-06-25 13:39:53 +0000

    remove `200 OK` checks as they are ensured by curl

--- sitemap-authors-xml/check/hook.pm
+++ sitemap-authors-xml/check/hook.pm
@@ -2,7 +2,7 @@ run_swat_module( GET => "sitemap-authors.xml.gz" );
 
 my $j=0;
 for my $i (our @site_map_url){
-  last if $j++>3;
+  #last if $j++>3;
   run_swat_module( GET => "sitemap-authors-xml/check-author" , { url => $i });
 }
 
