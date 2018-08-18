b0c9650ebe04d4e059922d5a8ff3fef99b4648ab
melezhik <melezhik@localhost.localdomain>
2018-08-17 23:06:29 -0500

    add feed alive meta story

--- hook.pm
+++ hook.pm
@@ -2,5 +2,5 @@ use metacpan::monitoring;
 
 run_swat_module( GET => '/recent' );
 run_swat_module( GET => '/search' );
-run_swat_module( GET => '/feed/recent' );
+run_swat_module( META => '/feed-alive' );
 run_swat_module( META => 'sitemap-authors-xml/check' );
